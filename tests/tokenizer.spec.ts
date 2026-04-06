3616dd0bd5c2ff8cdf9df877e339d7489c54a28c
Jefte Costa <jefteamorim@gmail.com>
2026-04-06 11:57:24 -0300

    test: add initial tokenizer tests

---
+++ tests/tokenizer.spec.ts
@@ -0,0 +1,42 @@
+import test from 'node:test'
+import assert from 'node:assert/strict'
+
+import { tokenize } from '../src/tokenizer.js'
+
+test('tokenize should parse escaped expression', () => {
+  const tokens = tokenize('Hello {{ name }}')
+
+  assert.equal(tokens.length, 2)
+  assert.deepEqual(tokens[0], { type: 'text', value: 'Hello ' })
+  assert.deepEqual(tokens[1], { type: 'expr', value: 'name', escaped: true })
+})
+
+test('tokenize should parse raw expression', () => {
+  const tokens = tokenize('{{{ html }}}')
+
+  assert.equal(tokens.length, 1)
+  assert.deepEqual(tokens[0], { type: 'expr', value: 'html', escaped: false })
+})
+
+test('tokenize should parse if directive', () => {
+  const tokens = tokenize('@if(user.loggedIn)\n<p>ok</p>\n@end')
+
+  assert.equal(tokens[0]?.type, 'directive')
+  assert.equal(tokens[0]?.value, '@if(user.loggedIn)')
+  assert.equal(tokens[tokens.length - 1]?.type, 'directive')
+  assert.equal(tokens[tokens.length - 1]?.value, '@end')
+})
+
+test('tokenize should parse each directive', () => {
+  const tokens = tokenize('@each(post in posts)\n{{ post }}\n@end')
+
+  assert.equal(tokens[0]?.type, 'directive')
+  assert.equal(tokens[0]?.value, '@each(post in posts)')
+})
+
+test('tokenize should parse component directive', () => {
+  const tokens = tokenize('@ui.button({ type: \'submit\' })\nSalvar\n@end')
+
+  assert.equal(tokens[0]?.type, 'directive')
+  assert.equal(tokens[0]?.value, '@ui.button({ type: \'submit\' })')
+})
